31dde382f4897e99003692b9cf02e097d5b227d4
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-04-01 23:06:44 +0200

    Set up scan screen

--- app/screens/scan/scan-screen.tsx
+++ app/screens/scan/scan-screen.tsx
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { observer } from 'mobx-react-lite'
-import { ViewStyle } from 'react-native'
-import { Screen, Text } from '../../components'
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "../../models"
-import { color } from '../../theme'
+import { StyleSheet, ViewStyle } from 'react-native'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import { Screen } from '../../components'
+import { Text } from '@ui-kitten/components'
+import { useStores } from '../../models'
+import { useNavigation } from '@react-navigation/native'
 
 const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
   flex: 1,
 }
 
 export const ScanScreen = observer(function ScanScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  const [hasPermission, setHasPermission] = useState(null)
+  const [scanned, setScanned] = useState(false)
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+  const { itemStore } = useStores()
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    ;(async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    })()
+  }, [])
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true)
+    console.log(`Bar code with type ${type} and data ${data} has been scanned!`)
+    // TODO: open Register or Info screen
+  }
+
+  // TODO: translate
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>
+  }
+
   return (
-    <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="Scan" />
+    <Screen style={ROOT} preset="fixed">
+      <BarCodeScanner
+        barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
     </Screen>
   )
 })
