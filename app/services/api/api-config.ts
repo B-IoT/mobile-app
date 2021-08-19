97e26eb7285632be3b22ffba82be605a35ad1ba1
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-08-19 16:00:57 +0200

    Use port 443 for the API

--- app/services/api/api-config.ts
+++ app/services/api/api-config.ts
@@ -1,7 +1,7 @@
 // Use this import if you want to use "env.js" file
 // const { API_URL } = require("../../config/env")
 // Or just specify it directly like this:
-const API_URL = 'https://api.b-iot.ch:8080'
+const API_URL = 'https://api.b-iot.ch:443'
 
 /**
  * The options used to configure the API.
